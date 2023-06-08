22dd5c9c98fbf061e051ba98eda2a0a5415a8a15
yashdev9274 <yashdev.yvd@gmail.com>
2023-06-08 17:29:05 +0530

    create user.js file

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const db = require('../utils/databse.js');
+
+const User = db.define('user', {
+
+
+    id:{
+
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+
+    },
+
+    name: Sequelize.STRING,
+    email: Sequelize.STRING
+
+
+});
+
+module.exports = User;
