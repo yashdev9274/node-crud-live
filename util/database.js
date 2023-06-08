778fcea5034d5dd2619be0c919b72f58de9cf45a
yashdev9274 <yashdev.yvd@gmail.com>
2023-06-08 17:19:38 +0530

    create database.js file

--- util/database.js
+++ util/database.js
@@ -3,4 +3,12 @@ const Sequelize  = require('sequelize');
 const sequelize = new Sequelize(
 
     process.env.PG_DB,
-)
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    {
+        host: process.env>PG_HOST,
+        dialect: 'podtgres',
+    }
+);
+
+module.export =  sequelize;
